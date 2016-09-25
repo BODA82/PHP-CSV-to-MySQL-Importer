1003ca905ef9379056cec5fdd1927983348b3e20
Christopher Spires <cspires@gmail.com>
2016-09-24 20:59:11 -0400

    Updated to allow for multiple columns

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
 # CSV-to-MySQL-Converter
-A simple PHP/PDO script to upload a CSV file to a single MySQL database table
+A simple PHP/PDO script to upload a CSV file to an existing MySQL database.
 
 ## Usage
+* Prepare your database
+ * All table columns must be setup prior to running this script
+* Prepare your CSV file
+ * The first row of your CSV file must contain the column name exactly as it exists in your database
 * Upload csv-msql-converter.php to location of your choice
 * Upload the CSV file you wish to convert to the same directory
 * Update the following variables with your database and CSV file information:
- * `$databasehost` (your database hostname, IP address, or localhost)
- * `$databasename` (name of your MySQL database)
- * `$databasetable` (which table in your database to place the data)
- * `$databaseusername` (username with permissions to the database)
- * `$databasepassword` (password for the database user)
- * `$fieldseperator` (file column separator/delimiter)
- * `$lineseparator` (file record line separator)
- * `$csvfile` (file name or full path if file is not located in same directory)
+ * `$database_host` (your database hostname, IP address, or localhost)
+ * `$database_name` (name of your MySQL database)
+ * `$database_table` (which table in your database to place the data)
+ * `$database_username` (username with permissions to the database)
+ * `$database_password` (password for the database user)
+ * `$csv_file` (file name or full path if file is not located in same directory)
 * Execute csv-mysql-converter.php
 
 ## License
--- csv-mysql-converter.php
+++ csv-mysql-converter.php
@@ -1,32 +1,47 @@
 <?php
-$databasehost = 'localhost'; 
-$databasename = 'databasename'; 
-$databasetable = 'databasetable'; 
-$databaseusername='databaseusername'; 
-$databasepassword = 'databasepassword'; 
-$fieldseparator = ","; 
-$lineseparator = "\n";
-$csvfile = 'filename.csv';
+$database_host = 'localhost'; 
+$database_name = 'databasename'; 
+$database_table = 'databasetable'; 
+$database_username='databaseusername'; 
+$database_password = 'databasepassword'; 
+$csv_file = 'filename.csv';
 
-if(!file_exists($csvfile)) {
-    die("File not found. Make sure you specified the correct path.");
+if (!file_exists($csv_file)) {
+    die('File not found. Make sure you specified the correct path.');
 }
 
-try {
-    $pdo = new PDO("mysql:host=$databasehost;dbname=$databasename", 
-        $databaseusername, $databasepassword,
+try {	
+    $pdo = new PDO("mysql:host=$database_host;dbname=$database_name", 
+        $database_username, $database_password,
         array(
-            PDO::MYSQL_ATTR_LOCAL_INFILE => true,
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         )
     );
 } catch (PDOException $e) {
-    die("database connection failed: ".$e->getMessage());
+    die('database connection failed: ' . $e->getMessage());
 }
 
-$affectedRows = $pdo->exec("
-    LOAD DATA LOCAL INFILE ".$pdo->quote($csvfile)." INTO TABLE `$databasetable`
-      FIELDS TERMINATED BY ".$pdo->quote($fieldseparator)."
-      LINES TERMINATED BY ".$pdo->quote($lineseparator));
+$input = fopen($csv_file, 'a+');
+$input2 = fopen($csv_file, 'a+');
+$first_row = fgetcsv($input, 1024, ',');
 
-echo "Loaded a total of $affectedRows records from this csv file.\n";
+foreach ($first_row as $name) {
+	$values_array[] = ':' . trim($name);
+}
+
+$columns = implode(', ', fgetcsv($input2, 1024, ','));
+$values = implode(', ', $values_array);
+
+$count = 0;
+while ($row = fgetcsv($input, 1024, ',')) {
+	$sql = "INSERT INTO $database_table($columns) VALUES($values)";
+	$query = $pdo->prepare($sql);
+	for ($i=0; $i < count($row); $i++) {
+		$query->bindParam($values_array[$i], $row[$i]);	
+	}
+	$query->execute();
+	$count++;
+}
+
+echo 'Loaded a total of ' . $count . ' records from your CSV file.';
